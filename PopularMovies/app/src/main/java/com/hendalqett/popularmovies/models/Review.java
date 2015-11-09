5fdd9991f945fe4192aec00c73ddd27dc3104cb1
Hend <cathe.2007@gmail.com>
2015-11-09 14:54:23 +0200

    Make classes parcelable

--- PopularMovies/app/src/main/java/com/hendalqett/popularmovies/models/Review.java
+++ PopularMovies/app/src/main/java/com/hendalqett/popularmovies/models/Review.java
@@ -1,16 +1,38 @@
 package com.hendalqett.popularmovies.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by hend on 11/8/15.
  */
-public class Review {
+public class Review implements Parcelable{
 
     @SerializedName("author")
     String author;
     @SerializedName("content")
     String content;
+
+    private Review(Parcel in) {
+        author= in.readString();
+        content = in.readString();
+
+
+    }
+
+    public static final Creator<Review> CREATOR = new Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel in) {
+            return new Review(in);
+        }
+
+        @Override
+        public Review[] newArray(int size) {
+            return new Review[size];
+        }
+    };
 
     public String getAuthor() {
         return author;
@@ -26,5 +48,16 @@ public class Review {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(author);
+        dest.writeString(content);
     }
 }
--- PopularMovies/app/src/main/java/com/hendalqett/popularmovies/models/Trailer.java
+++ PopularMovies/app/src/main/java/com/hendalqett/popularmovies/models/Trailer.java
@@ -1,16 +1,36 @@
 package com.hendalqett.popularmovies.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by hend on 11/8/15.
  */
-public class Trailer {
+public class Trailer implements Parcelable {
 
     @SerializedName("key")
     String key;
     @SerializedName("name")
     String name;
+
+    protected Trailer(Parcel in) {
+        key = in.readString();
+        name = in.readString();
+    }
+
+    public static final Creator<Trailer> CREATOR = new Creator<Trailer>() {
+        @Override
+        public Trailer createFromParcel(Parcel in) {
+            return new Trailer(in);
+        }
+
+        @Override
+        public Trailer[] newArray(int size) {
+            return new Trailer[size];
+        }
+    };
 
     public String getKey() {
         return key;
@@ -26,5 +46,16 @@ public class Trailer {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(key);
+        dest.writeString(name);
     }
 }
